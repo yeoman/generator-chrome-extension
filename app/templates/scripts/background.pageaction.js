056ea93cdc38952d98746e4eadc22f4eab17e993
ragingwind <ragingwind@gmail.com>
2013-02-25 22:50:03 +0900

    add template file for background.js

--- app/templates/scripts/background.pageaction.js
+++ app/templates/scripts/background.pageaction.js
@@ -6,3 +6,7 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId) {
+    chrome.pageAction.show(tabId);
+});
