1887fb4d689c54f10dc73c9d5cb6cf54b25877af
Kevin Martensson <kevinmartensson@gmail.com>
2013-04-07 16:01:02 +0200

    Code consistency

--- app/index.js
+++ app/index.js
@@ -4,10 +4,10 @@ var util    = require('util');
 var yeoman  = require('yeoman-generator');
 
 var ChromeExtensionGenerator = module.exports = function ChromeExtensionGenerator (args, options, config) {
-  yeoman.generators.Base.apply( this, arguments );
+  yeoman.generators.Base.apply(this, arguments);
 
   // set source root path to templates
-  this.sourceRoot( path.join( __dirname, 'templates') );
+  this.sourceRoot(path.join(__dirname, 'templates'));
 
   // init extension manifest data
   this.manifest = {
@@ -32,9 +32,9 @@ var ChromeExtensionGenerator = module.exports = function ChromeExtensionGenerato
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
-util.inherits( ChromeExtensionGenerator, yeoman.generators.NamedBase );
+util.inherits(ChromeExtensionGenerator, yeoman.generators.NamedBase);
 
-ChromeExtensionGenerator.prototype.askFor = function askFor( argument ) {
+ChromeExtensionGenerator.prototype.askFor = function askFor (argument) {
   var cb = this.async();
 
   var prompts = [
@@ -106,28 +106,28 @@ ChromeExtensionGenerator.prototype.askFor = function askFor( argument ) {
     }
   ];
 
-  this.prompt( prompts , function( err, props ) {
+  this.prompt( prompts , function(err, props) {
     if (err) {
-      return this.emit( 'error', err );
+      return this.emit('error', err);
     }
 
     this.appname = this.manifest.name = props.name;
     this.manifest.description = props.description;
-    this.manifest.action = ((/1|2/).test( props.action )) ? Math.floor( props.action ) : 0;
-    this.manifest.options = !(/n/i).test( props.options );
+    this.manifest.action = ((/1|2/).test(props.action)) ? Math.floor(props.action) : 0;
+    this.manifest.options = !(/n/i).test(props.options);
     this.manifest.omnibox = props.omnibox;
-    this.manifest.contentscript = !(/n/i).test( props.contentscript );
-    this.manifest.permissions.tabs = !(/n/i).test( props.tabs );
-    this.manifest.permissions.bookmarks = !(/n/i).test( props.bookmark );
-    this.manifest.permissions.cookies = !(/n/i).test( props.cookie );
-    this.manifest.permissions.history = !(/n/i).test( props.history );
-    this.manifest.permissions.management = !(/n/i).test( props.management );
+    this.manifest.contentscript = !(/n/i).test(props.contentscript);
+    this.manifest.permissions.tabs = !(/n/i).test(props.tabs);
+    this.manifest.permissions.bookmarks = !(/n/i).test(props.bookmark);
+    this.manifest.permissions.cookies = !(/n/i).test(props.cookie);
+    this.manifest.permissions.history = !(/n/i).test(props.history);
+    this.manifest.permissions.management = !(/n/i).test(props.management);
 
     cb();
-  }.bind( this ));
+  }.bind(this));
 };
 
-ChromeExtensionGenerator.prototype.manifestFiles = function manifestFiles() {
+ChromeExtensionGenerator.prototype.manifestFiles = function manifestFiles () {
   var manifest = {};
   var permissions = [];
   var items = [];
@@ -140,7 +140,7 @@ ChromeExtensionGenerator.prototype.manifestFiles = function manifestFiles() {
       default_popup: 'popup.html'
     };
     var title = (this.manifest.action === 1) ? 'browser_action' : 'page_action';
-    manifest[title] = JSON.stringify( action, null, 2 ).replace( /\n/g, '\n  ' );
+    manifest[title] = JSON.stringify(action, null, 2).replace(/\n/g, '\n  ');
   }
 
   // add options page field.
@@ -150,7 +150,7 @@ ChromeExtensionGenerator.prototype.manifestFiles = function manifestFiles() {
 
   // add omnibox keyword field.
   if (this.manifest.omnibox) {
-    manifest.omnibox = JSON.stringify({ keyword: this.manifest.omnibox }, null, 2 ).replace( /\n/g, '\n  ' );
+    manifest.omnibox = JSON.stringify({ keyword: this.manifest.omnibox }, null, 2).replace(/\n/g, '\n  ');
   }
 
   // add contentscript field.
@@ -163,44 +163,44 @@ ChromeExtensionGenerator.prototype.manifestFiles = function manifestFiles() {
       all_frames: false
     }];
 
-    manifest.content_scripts = JSON.stringify( contentscript, null, 2 ).replace( /\n/g, '\n  ' );
+    manifest.content_scripts = JSON.stringify(contentscript, null, 2).replace(/\n/g, '\n  ');
   }
 
   // add generate permission field.
   for (var p in this.manifest.permissions) {
     if (this.manifest.permissions[p]) {
-      permissions.push( p );
+      permissions.push(p);
     }
   }
 
   // add generic match pattern field.
   if (this.manifest.permissions.tabs) {
-    permissions.push( 'http://*/*' );
-    permissions.push( 'https://*/*' );
+    permissions.push('http://*/*');
+    permissions.push('https://*/*');
   }
 
   if (permissions.length > 0) {
-    manifest.permissions = JSON.stringify( permissions, null, 2 ).replace( /\n/g, '\n  ' );
+    manifest.permissions = JSON.stringify(permissions, null, 2).replace(/\n/g, '\n  ');
   }
 
   for (var i in manifest) {
-    items.push(['  "', i, '": ', manifest[i]].join( '' ));
+    items.push(['  "', i, '": ', manifest[i]].join(''));
   }
 
-  this.manifest.items = (items.length > 0) ? ',\n' + items.join( ',\n' ) : '';
+  this.manifest.items = (items.length > 0) ? ',\n' + items.join(',\n') : '';
 
-  this.template( 'manifest.json', 'app/manifest.json' );
+  this.template('manifest.json', 'app/manifest.json');
 };
 
-ChromeExtensionGenerator.prototype.extensionFiles = function extensionFiles() {
+ChromeExtensionGenerator.prototype.extensionFiles = function extensionFiles () {
   var backgroundjs = 'background.js';
 
   // browser or page action files.
   if (this.manifest.action > 0) {
-    this.template( 'popup.html', 'app/popup.html' );
-    this.template( 'scripts/popup.js', 'app/scripts/popup.js' );
-    this.copy( 'images/icon-19.png', 'app/images/icon-19.png' );
-    this.copy( 'images/icon-38.png', 'app/images/icon-38.png' );
+    this.template('popup.html', 'app/popup.html');
+    this.template('scripts/popup.js', 'app/scripts/popup.js');
+    this.copy('images/icon-19.png', 'app/images/icon-19.png');
+    this.copy('images/icon-38.png', 'app/images/icon-38.png');
     if (this.manifest.action === 2) {
       backgroundjs = 'background.pageaction.js';
     }
@@ -208,33 +208,33 @@ ChromeExtensionGenerator.prototype.extensionFiles = function extensionFiles() {
 
   // options files
   if (this.manifest.options) {
-    this.template( 'options.html', 'app/options.html' );
-    this.template( 'scripts/options.js', 'app/scripts/options.js' );
+    this.template('options.html', 'app/options.html');
+    this.template('scripts/options.js', 'app/scripts/options.js');
   }
 
   // content script
   if (this.manifest.contentscript) {
-    this.template( 'scripts/contentscript.js', 'app/scripts/contentscript.js' );
+    this.template('scripts/contentscript.js', 'app/scripts/contentscript.js');
   }
 
   // background script
-  this.template( 'scripts/' + backgroundjs, 'app/scripts/background.js' );
+  this.template('scripts/' + backgroundjs, 'app/scripts/background.js');
 
   // extension assets
-  this.template( '_locales/en/messages.json', 'app/_locales/en/messages.json' );
-  this.copy( 'styles/main.css', 'app/styles/main.css' );
-  this.copy( 'images/icon-16.png', 'app/images/icon-16.png' );
-  this.copy( 'images/icon-128.png', 'app/images/icon-128.png' );
+  this.template('_locales/en/messages.json', 'app/_locales/en/messages.json');
+  this.copy('styles/main.css', 'app/styles/main.css');
+  this.copy('images/icon-16.png', 'app/images/icon-16.png');
+  this.copy('images/icon-128.png', 'app/images/icon-128.png');
 };
 
 
-ChromeExtensionGenerator.prototype.packageFiles = function packageFiles() {
-  this.copy( 'package.json', 'package.json' );
-  this.copy( 'component.json', 'component.json' );
-  this.copy( 'bowerrc', '.bowerrc' );
-  this.copy( 'editorconfig', '.editorconfig' );
-  this.copy( 'gitignore', '.gitignore' );
-  this.copy( 'gitattributes', '.gitattributes' );
-  this.copy( 'jshintrc', '.jshintrc' );
-  this.template( 'Gruntfile.js' );
+ChromeExtensionGenerator.prototype.packageFiles = function packageFiles () {
+  this.copy('package.json', 'package.json');
+  this.copy('component.json', 'component.json');
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+  this.copy('jshintrc', '.jshintrc');
+  this.template('Gruntfile.js');
 };
