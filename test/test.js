99ef39783b1e33868c7c558fae434117fcb014d0
ragingwind <ragingwind@gmail.com>
2013-04-15 23:53:36 +0900

    #9

--- test/test.js
+++ test/test.js
@@ -6,7 +6,176 @@ var assert = require('assert');
 var helpers = require('yeoman-generator').test;
 
 describe('Chrome Extension generator test', function () {
-  it('fixme', function () {
-    assert(true);
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      this.extension = helpers.createGenerator('chrome-extension:app', [
+        '../../app', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
+      ]);
+      done();
+    }.bind(this));
   });
+
+  it('the generator can be required without throwing', function () {
+    // not testing the actual run of generators yet
+    this.app = require('../app');
+  });
+
+  it('creates expected files in no UI Action', function (done) {
+    var expected = [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      'app/manifest.json',
+      'app/_locales/en/messages.json',
+      'app/images/icon-128.png',
+      'app/images/icon-16.png',
+      'app/styles/main.css'
+    ];
+
+    helpers.mockPrompt(this.extension, {
+      'name': 'temp',
+    });
+
+    this.extension.options['skip-install'] = true;
+    this.extension.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('creates expected files in Browser Action', function (done) {
+    var expected = [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      ['app/manifest.json', /"browser_action": {\s+"default_icon": {\s+"19": "images\/icon-19.png",\s+"38": "images\/icon-38.png"\s+},\s+"default_title": "temp",\s+"default_popup": "popup.html"\s+}/],
+      'app/_locales/en/messages.json',
+      'app/images/icon-128.png',
+      'app/images/icon-16.png',
+      'app/images/icon-19.png',
+      'app/images/icon-38.png',
+      'app/scripts/background.js',
+      'app/scripts/popup.js',
+      'app/popup.html',
+      'app/styles/main.css'
+    ];
+
+    helpers.mockPrompt(this.extension, {
+      'name': 'temp',
+      'action': '1'
+    });
+
+    this.extension.options['skip-install'] = true;
+    this.extension.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('creates expected files in Page Action', function (done) {
+    var expected = [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      ['app/manifest.json', /"page_action": {\s+"default_icon": {\s+"19": "images\/icon-19.png",\s+"38": "images\/icon-38.png"\s+},\s+"default_title": "temp",\s+"default_popup": "popup.html"\s+}/],
+      'app/_locales/en/messages.json',
+      'app/images/icon-128.png',
+      'app/images/icon-16.png',
+      'app/images/icon-19.png',
+      'app/images/icon-38.png',
+      'app/scripts/background.js',
+      'app/scripts/popup.js',
+      'app/popup.html',
+      'app/styles/main.css'
+    ];
+
+    helpers.mockPrompt(this.extension, {
+      'name': 'temp',
+      'action': '2'
+    });
+
+    this.extension.options['skip-install'] = true;
+    this.extension.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('creates expected files with Options Page', function (done) {
+    var expected = [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      ['app/manifest.json', /"options_page": "options.html"/],
+      'app/_locales/en/messages.json',
+      'app/images/icon-128.png',
+      'app/images/icon-16.png',
+      'app/scripts/background.js',
+      'app/scripts/options.js',
+      'app/options.html',
+      'app/styles/main.css'
+    ];
+
+    helpers.mockPrompt(this.extension, {
+      'name': 'temp',
+      'options': 'Y'
+    });
+
+    this.extension.options['skip-install'] = true;
+    this.extension.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('creates manifest.json with Omnibox option', function (done) {
+    var expected = [
+      ['app/manifest.json', /"omnibox": {\s+"keyword": "omnibox"\s+}/]
+    ];
+
+    helpers.mockPrompt(this.extension, {
+      'name': 'temp',
+      'omnibox': 'omnibox'
+    });
+
+    this.extension.options['skip-install'] = true;
+    this.extension.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('creates expected files with Content-script option', function (done) {
+    var expected = [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      ['app/manifest.json', /"content_scripts": \[\s+{\s+"matches": \[\s+"http:\/\/\*\/\*",\s+"https:\/\/\*\/\*"\s+\],\s+"css": \[\s+"styles\/main.css"\s+\],\s+"js": \[\s+"scripts\/contentscript.js"\s+\],\s+"run_at": "document_end",\s+"all_frames": false/],
+      'app/_locales/en/messages.json',
+      'app/images/icon-128.png',
+      'app/images/icon-16.png',
+      'app/scripts/background.js',
+      'app/scripts/contentscript.js',
+      'app/styles/main.css'
+    ];
+
+    helpers.mockPrompt(this.extension, {
+      'name': 'temp',
+      'contentscript': 'Y'
+    });
+
+    this.extension.options['skip-install'] = true;
+    this.extension.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
 });
