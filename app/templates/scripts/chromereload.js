337edaa7b40fa9bbdec597c0a6b6d61d0cca2f91
Rosina Bignall <bignall@users.noreply.github.com>
2017-10-12 05:44:31 -0700

    Reload the extension as well as the pages (#174)
    * Reload the extension as well as the pages
    
    chrome.runtime.reload() doesn't actually reload the extension from the source. I solved this by adding chrome.developerPrivate.reload(...). Now when you change the files and livereload is triggered the extension reloads from the source.
    
    * Only allow reloads once a minute to avoid the browser disabling the extension for too frequent reloads

--- app/templates/scripts/chromereload.js
+++ app/templates/scripts/chromereload.js
@@ -8,6 +8,12 @@ const LIVERELOAD_HOST = 'localhost:';
 const LIVERELOAD_PORT = 35729;
 const connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
 
+var lastReload = false;
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  lastReload = Date.now();
+});    
+
 connection.onerror = error => {
   console.log('reload connection got error:', error);
 };
@@ -16,7 +22,13 @@ connection.onmessage = e => {
   if (e.data) {
     const data = JSON.parse(e.data);
     if (data && data.command === 'reload') {
-      chrome.runtime.reload();
+      var currentTime = Date.now();
+      if (lastReload && currentTime - lastReload > 60000) {
+	// don't reload more than once a minute
+	chrome.runtime.reload();
+	chrome.developerPrivate.reload(chrome.runtime.id, 
+				       {failQuietly: true});
+      }
     }
   }
 };
@@ -24,6 +36,12 @@ connection.onmessage = e => {
 var LIVERELOAD_HOST = 'localhost:';
 var LIVERELOAD_PORT = 35729;
 var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
+
+var lastReload = false;
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  lastReload = Date.now();
+});    
 
 connection.onerror = function (error) {
   console.log('reload connection got error:', error);
@@ -33,7 +51,12 @@ connection.onmessage = function (e) {
   if (e.data) {
     var data = JSON.parse(e.data);
     if (data && data.command === 'reload') {
-      chrome.runtime.reload();
+      if (lastReload && currentTime - lastReload > 60000) {
+	// don't reload more than once a minute
+	chrome.runtime.reload();
+	chrome.developerPrivate.reload(chrome.runtime.id, 
+				       {failQuietly: true});
+      }
     }
   }
 };
